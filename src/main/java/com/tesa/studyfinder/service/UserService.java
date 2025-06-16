36cf397d0898656689fb7771610d46c307589e78
Samuel <samadeolu7@gmail.com>
2025-06-16 08:08:50 +0100

    Edit: Added password encoder

--- src/main/java/com/tesa/studyfinder/service/UserService.java
+++ src/main/java/com/tesa/studyfinder/service/UserService.java
@@ -3,8 +3,6 @@ package com.tesa.studyfinder.service;
 import com.tesa.studyfinder.model.User;
 import com.tesa.studyfinder.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -37,9 +35,4 @@ public class UserService {
                 .filter(u -> passwordEncoder.matches(rawPassword, u.getPasswordHash()));
     }
 
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
